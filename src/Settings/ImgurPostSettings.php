3abb8dfdbd29038b44913db70916bec771a57b7a
neerajp99 <neerajp1999@gmail.com>
2019-05-18 13:24:32 +0530

    add settings file;

---
+++ src/Settings/ImgurPostSettings.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Drupal\social_post_imgur\Settings;
+
+use Drupal\social_api\Settings\SettingsBase;
+
+/**
+ * Returns the Imgur app information.
+ */
+class ImgurPostSettings extends SettingsBase implements ImgurPostSettingsInterface {
+
+  /**
+   * Clients ID.
+   *
+   * @var string
+   */
+  protected $clientId;
+
+  /**
+   * Client secret.
+   *
+   * @var string
+   */
+  protected $clientSecret;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getClientId() {
+    if (!$this->clientId) {
+      $this->clientId = $this->config->get('client_id');
+    }
+    return $this->clientId;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getClientSecret() {
+    if (!$this->clientSecret) {
+      $this->clientSecret = $this->config->get('client_secret');
+    }
+    return $this->clientSecret;
+  }
+
+}
